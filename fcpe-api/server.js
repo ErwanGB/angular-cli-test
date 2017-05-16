437860bc6a40d39417a047673853a980ae63b32f
Erwan <erwanguy@gmail.com>
2017-05-16 10:42:31 +0200

    paths

--- fcpe-api/server.js
+++ fcpe-api/server.js
@@ -4,14 +4,14 @@ const app = express();
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static('../dist'));
+app.use(express.static(__dirname + '/../dist'));
 
 const path = require('path');
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('../dist/index.html'));
+  res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
 
 // Start the app by listening on the default
